f2427a37ac22af068b57d2ed343eb55ea2903161
Erik Pérez <erikxp@gmail.com>
2025-09-26 16:15:32 -0300

    version update #116

--- version.php
+++ version.php
@@ -33,7 +33,7 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024061801;
+$plugin->version   = 2024061801;# The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051703;
 $plugin->component = 'local_coursetransfer';
 $plugin->release   = '1.1.1';
